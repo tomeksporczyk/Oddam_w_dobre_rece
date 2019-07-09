0ce3c8222b5b4054db13d5e16e411be17a3e9626
Tomek <tomeksporczyk@gmail.com>
2019-07-09 11:16:13 +0200

    4 basic views with linked styles

--- OWDR/urls.py
+++ OWDR/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from OWDR.views import LandingPageView
+from OWDR.views import *
 
 urlpatterns = [
     path('', LandingPageView.as_view(), name='landing_page'),
-    path('login', LoginView.as_view(), name = 'login')
+    path('login', LoginView.as_view(), name='login'),
+    path('register', RegisterView.as_view(), name='register'),
+    path('form', FormView.as_view(), name='form'),
 ]
--- OWDR/views.py
+++ OWDR/views.py
@@ -4,3 +4,21 @@ from django.shortcuts import render
 from django.views import View
 
 
+class LandingPageView(View):
+    def get(self, request):
+        return render(request, 'OWDR/index.html')
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'OWDR/login.html')
+
+
+class RegisterView(View):
+    def get(self, request):
+        return render(request, 'OWDR/register.html')
+
+
+class FormView(View):
+    def get(self, request):
+        return render(request, 'OWDR/form.html')
